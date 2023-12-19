f14a802d83be518a3d832a1a1f40cf14a7aa1c3c
louay <louaysakli65@gmail.com>
2023-12-19 21:31:09 +0100

    productcontoller and productroutes

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,39 @@
+y// routes/products.js
+const express = require('express');
+
+
+// controllers/productsController.js
+const Product = require('../database/models/productsModel.js');
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const getProductById = async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const product = await Product.findByPk(productId);
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
+
--- backend/database/config.js
+++ backend/database/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("ecommercewebsite", "root", "db password", {
+const sequelize = new Sequelize("ecommercewebsite", "root", "Louay2004@", {
   dialect: "mysql",
   host: "localhost",
 });
