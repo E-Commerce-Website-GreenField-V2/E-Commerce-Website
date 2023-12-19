4b056bfd8c4e446ab72473c20b7c278e22e03a73
salmenkhelifi1 <salmen@base.tn>
2023-12-19 15:53:26 +0100

    init

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const db = require("./database/index.js");
+const appRoutes = require("./routes");
+const PORT = 3000;
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+app.use(express.static(__dirname + "/../client/dist"));
+//auth routs
+app.use("/signup", authRoutes);
+app.use("/app", authMiddelware, appRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
