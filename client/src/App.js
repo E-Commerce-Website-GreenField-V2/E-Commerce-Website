6063a7dfacd95147166e2717215c8046223db2e2
louay <louaysakli65@gmail.com>
2023-12-20 09:49:20 +0100

    product details front

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import ProductDetailsPage from "./components/ProductDetails.jsx";
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <ProductDetailsPage />
+    </div>
+  );
 }
 
 export default App;
