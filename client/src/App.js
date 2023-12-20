f7940192def767f65b639538ef200d967f68ed41
louay <louaysakli65@gmail.com>
2023-12-20 16:17:54 +0100

    contact and about

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetailsPage from "./components/products/ProductDetails.jsx";
 import Account from "./components/user/Account.jsx";
 import "./App.css";
+import Contact from "./components/static/Contact.jsx";
+import About from "./components/static/About.jsx"
+
 
 function App() {
   return (
@@ -10,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/account" element={<Account />} />
         <Route path="/ProductDetails" element={<ProductDetailsPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
