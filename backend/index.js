874c82055d0beddcbafd1ec5bb43f8452ba0bd8e
salmenkhelifi1 <salmen@base.tn>
2023-12-20 14:41:34 +0100

    categorey

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,13 @@ const db = require("./database/index.js");
 const PORT = 8000;
 const app = express();
 const cors = require("cors");
-const productsRouter = require('./routes/ProductRoutes.js');
-const usersRouter = require('./routes/userRoutes.js');
-const {authenticateUser} = require ('./middelwares/auth.js')
-const authRoute = require('./routes/authRoute.js')
+const productsRouter = require("./routes/ProductRoutes.js");
+const usersRouter = require("./routes/userRoutes.js");
+const { authenticateUser } = require("./middelwares/auth.js");
+const authRoute = require("./routes/authRoute.js");
+const categoryRoutes = require("./routes/categoryRoute.js");
 
 app.use(express.json());
-
-
-
 
 app.use(cors());
 app.use(express.static(__dirname + "/../client/dist"));
@@ -21,9 +19,9 @@ app.use("/auth", authRoute);
 
 //all routes below this middelware are secure
 // app.use(authenticateUser);
-
-app.use('/products', productsRouter); 
-app.use('/users', usersRouter);
+app.use("/categories", categoryRoutes);
+app.use("/products", productsRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
