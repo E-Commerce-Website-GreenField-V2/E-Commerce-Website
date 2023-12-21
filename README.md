5ebc85cb6430f51ae89ee14bdeacd9f046a7a7e4
salmenkhelifi1 <salmen@base.tn>
2023-12-21 14:53:30 +0100

    app

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# E-Commerce-Website
+
+# MERN E-Commerce-Website
+
+<code>Currently under construction for additional features</code>
+
+# Description
+
+Full-stack ecommerce application built with MERN stack. This project has thre features:
+
+1. Buyers register and browse the marketplace while interacting with products across different categories.
+2. Admins control and manage the marketplace items and customer acounts.
+3. Salers register and browse the Marketplace items and seles can control and manage ther items
+# Getting Started
+
+## Dependencies
+
+- Nodejs - The runtime environment of the application
+- Reactjs - Component based UI library
+- sequelize - ORM database
+- Expressjs - Framework to handle routes and requests
+
+## Installation
+
+After you cloned the repository do not start the application. To run the application fully you need to create a database and collection and provide your own database .
+
+### Setup
+
+```
+    NODEMON = development
+    PORT = 3000
+```
+
+### Running
+
+After this you can run the project locally:
+
+- To run the front end
+  - <code>cd client</code>
+  - <code>npm start</code>
+- To run the back end
+  - <code>cd server</code>
+  - <code>npm run dev</code>
+    TIP: Run both in split terminal so that you can see both ends running
+
+# Languages and tools
+
+- [Node](https://nodejs.org)
+- [React](https://reactjs.org/)
+- [Expressjs](https://expressjs.com)
+- [Sequelize](https://sequelize.org/)
+- [Material UI](https://mui.com/)
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Login from "./components/authentification/Login.jsx";
 import ProductDetailsPage from "./components/products/ProductDetails.jsx";
 import Account from "./components/user/Account.jsx";
@@ -7,18 +8,54 @@ import "./App.css";
 import Contact from "./components/static/Contact.jsx";
 import About from "./components/static/About.jsx";
 import NotFound from "./components/static/NotFound.jsx";
+import { AuthProvider } from "./Context/authContext.jsx";
+import Header from "./components/Header/Header.js";
+import Footer from "./components/Footer/Footer";
+import Home from "./routes/Home.jsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/account" element={<Account />} />
-        <Route path="/ProductDetails" element={<ProductDetailsPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/account">
+            {/* <Route path="me" element={<MyAccount/>}/>
+                <Route path="manage" element={<ManageAccount/>}/>
+                <Route path="login" element={<Login />}/>
+                <Route path="register" element={<Register />}/>
+                <Route path="*" element={<Login />}/> */}
+          </Route>
+          {/* <Route path="/shop" element={<Shop />} />
+          <Route path="/category">
+            <Route path=":id" element={<CategoryView />} />
+          </Route> */}
+          {/* <Route path="/item">
+            <Route path="/item/men">
+              <Route path=":id" element={<ItemView />} />
+            </Route>
+            <Route path="/item/women">
+              <Route path=":id" element={<ItemView />} />
+            </Route>
+            <Route path="/item/kids">
+              <Route path=":id" element={<ItemView />} />
+            </Route>
+            <Route path="/item/featured">
+              <Route path=":id" element={<ItemView />} />
+            </Route>
+          </Route> */}
+          {/* <Route path="/wishlist" element={<Wishlist />} /> */}
+          {/* <Route path="/search/*" element={<SearchView />} /> */}
+
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Routes>{/* <Route path="/admin" element={<Admin />} /> */}</Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
