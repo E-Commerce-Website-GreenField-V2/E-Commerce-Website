53739ac785072a7c03e91678f1893f202fbf90a7
amineguezmir <guezmiramine1@gmail.com>
2023-12-21 16:18:23 +0100

    account 80%

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Login from "./components/authentification/Login.jsx";
+// import Login from "./components/authentification/Login.jsx";
 import ProductDetailsPage from "./components/products/ProductDetails.jsx";
 import Account from "./components/user/Account.jsx";
 import "./App.css";
@@ -9,23 +9,25 @@ import Contact from "./components/static/Contact.jsx";
 import About from "./components/static/About.jsx";
 import NotFound from "./components/static/NotFound.jsx";
 import { AuthProvider } from "./Context/authContext.jsx";
-import Header from "./components/Header/Header.js";
-import Footer from "./components/Footer/Footer";
-import Home from "./routes/Home.jsx";
+import ManageAccount from "./components/Account/ManageAccount/ManageAccount.js";
+import MyAccount from "./components/Account/MyAccount/MyAccount.js";
+// import Header from "./components/Header/Header.js";
+// import Footer from "./components/Footer/Footer";
+// import Home from "./routes/Home.jsx";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Header />
+        {/* <Header /> */}
         <Routes>
-          <Route index element={<Home />} />
+          {/* <Route index element={<Home />} /> */}
           <Route path="/account">
-            {/* <Route path="me" element={<MyAccount/>}/>
-                <Route path="manage" element={<ManageAccount/>}/>
-                <Route path="login" element={<Login />}/>
-                <Route path="register" element={<Register />}/>
-                <Route path="*" element={<Login />}/> */}
+            <Route path="me" element={<MyAccount />} />
+            <Route path="manage" element={<ManageAccount />} />
+            {/* <Route path="login" element={<Login />}/> */}
+            {/* <Route path="register" element={<Register />}/> */}
+            {/* <Route path="*" element={<Login />}/>  */}
           </Route>
           {/* <Route path="/shop" element={<Shop />} />
           <Route path="/category">
@@ -52,7 +54,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
         <Routes>{/* <Route path="/admin" element={<Admin />} /> */}</Routes>
       </Router>
     </AuthProvider>
