caaa344f83d8b0e63d19a5a84666303630042967
louay <louaysakli65@gmail.com>
2023-12-22 18:53:45 +0100

    search

--- client/src/components/navbar/SearchBar/SearchBar.js
+++ client/src/components/navbar/SearchBar/SearchBar.js
@@ -1,33 +1,62 @@
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./SearchBar.css";
 import { useContext } from "react";
 import { SearchContext } from "../../../Context/SearchContext";
 
 const Form = () => {
   const [searchInput, setSearchInput] = useState("");
+  const [products, setProducts] = useState([]);
   const searchContext = useContext(SearchContext);
   const navigate = useNavigate();
 
-  const handelChange = (e) => {
+  useEffect(() => {
+    // Fetch products when the component mounts
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/products");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const responseData = await response.json();
+        setProducts(responseData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []); // Empty dependency array to fetch products only once 
+ 
+
+  const handleChange = (e) => {
     setSearchInput(e.target.value);
   };
 
-  const handelFormSubmit = (e) => {
+  const handleFormSubmit = (e) => {
     e.preventDefault();
-    searchContext.setSearchQuery(searchInput);
+
+    // Filter products based on the search input
+    const filteredProducts = products.filter((product) =>
+      product.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
+    // Update the search results in the context
+    searchContext.setSearchResults(filteredProducts);
+
+    // Redirect to the search results page
     navigate("/search");
   };
 
   return (
-    <form className="search__form" onSubmit={handelFormSubmit}>
+    <form className="search__form" onSubmit={handleFormSubmit}>
       <input
         type="text"
         placeholder="Search for products"
         className="search__form__input"
         value={searchInput}
-        onChange={handelChange}
+        onChange={handleChange}
         required
       />
       <button className="search__form__button" type="submit">
